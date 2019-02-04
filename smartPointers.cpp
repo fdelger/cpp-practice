ec0bc183d8d811a24e48058b37991b420ba5c02e
fdelger <fdelger@miami.edu>
2019-02-04 09:57:40 -0500

    fixed incorrect comments

--- smartPointers.cpp
+++ smartPointers.cpp
@@ -20,14 +20,14 @@ void disp (std::unique_ptr<int> & ptr) {
 int main( int argc, char ** argv) {
   message("create unique pointer");
   std::unique_ptr<int> ptrA = std::make_unique<int>(1); // make_unique better than using the new operator. try not to use new/delete.
-  std::unique_ptr<int> ptrC (new int (3));
+  std::unique_ptr<int> ptrC (new int (3)); // bad
   disp(ptrA);
   disp(ptrC);
   ptrC = std::move(ptrA);
   disp(ptrC);
 
   message("make a second unique ptr");
-  auto ptrB = std::make_unique<int>(2); // static storage, note auto use.
+  auto ptrB = std::make_unique<int>(2); 
 
   message("Print A, should be null because I moved it to ptrC");
   disp(ptrA);
